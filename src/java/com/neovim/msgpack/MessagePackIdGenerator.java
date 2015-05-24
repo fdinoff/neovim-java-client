8689efd65687979b3d3f1882ccc1da83a21111a2
Frank Dinoff <frank.dinoff@gmail.com>
2015-05-24 18:45:09 +0800

    Add documentation for MessagePackIdGenerator

--- src/java/com/neovim/msgpack/MessagePackIdGenerator.java
+++ src/java/com/neovim/msgpack/MessagePackIdGenerator.java
@@ -2,17 +2,31 @@ package com.neovim.msgpack;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Generator for MessagePack request ids. Requests ids must must be 32 bit unsigned integers.
+ */
 public class MessagePackIdGenerator {
     private final AtomicInteger id;
 
+    /**
+     * Generator that starts where the first Id is 0.
+     */
     public MessagePackIdGenerator() {
         this(0);
     }
 
+    /**
+     * Initialize the generator to the initialValue treated as an unsigned integer
+     * @param initialValue unsigned integer value used for initial id.
+     */
     public MessagePackIdGenerator(int initialValue) {
          id = new AtomicInteger(initialValue);
     }
 
+    /**
+     * Get the next id.
+     * @return next id where id is between 0 and 2^32 - 1 (32 bit unsigned integer range)
+     */
     public long nextId() {
         // Make sure Id is in the range of 0 to 2^32 - 1
         return Integer.toUnsignedLong(id.getAndIncrement());
