f045060a03ff43683b2f8407ddf100db1da4384b
Frank Dinoff <frank.dinoff@gmail.com>
2015-05-27 07:39:34 +0800

    Add more wrappers around vim functions
    Includes:
    vim_feedkeys
    vim_input
    vim_replace_termcodes
    vim_command_output
    vim_strwidth
    vim_list_runtime_paths
    vim_change_directory

--- src/main/java/com/neovim/Neovim.java
+++ src/main/java/com/neovim/Neovim.java
@@ -23,6 +23,41 @@ public class Neovim implements AutoCloseable {
         messagePackRPC.sendNotification(notification);
     }
 
+    public void feedKeys(String keys, String mode, boolean escapeCsi) throws IOException {
+        Notification notification = new Notification("vim_feedkeys", keys, mode, escapeCsi);
+        messagePackRPC.sendNotification(notification);
+    }
+
+    public Future<Long> sendInput(String input) throws IOException {
+        Request request = new Request("vim_input", input);
+        return messagePackRPC.sendRequest(request, Long.class);
+    }
+
+    public Future<String> replaceTermcodes(String str, boolean fromPart, boolean doLt, boolean special) throws IOException {
+        Request request = new Request("vim_replace_termcodes", str, fromPart, doLt, special);
+        return messagePackRPC.sendRequest(request, String.class);
+    }
+
+    public Future<String> commandOutput(String str) throws IOException {
+        Request request = new Request("vim_command_output", str);
+        return messagePackRPC.sendRequest(request, String.class);
+    }
+
+    public Future<Long> stringWidth(String str) throws IOException {
+        Request request = new Request("vim_strwidth", str);
+        return messagePackRPC.sendRequest(request, Long.class);
+    }
+
+    public Future<ArrayList<byte[]>> getRuntimePaths() throws IOException {
+        Request request = new Request("vim_list_runtime_paths");
+        return messagePackRPC.sendRequest(request, new TypeReference<ArrayList<byte[]>>() {});
+    }
+
+    public void changeDirectory(String directory) throws IOException {
+        Notification notification = new Notification("vim_change_directory", directory);
+        messagePackRPC.sendNotification(notification);
+    }
+
     @Override
     public void close() throws Exception {
         messagePackRPC.close();
