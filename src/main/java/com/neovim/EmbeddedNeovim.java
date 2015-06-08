662396fd07c3ff53d2b0c6a4c46d543ab6457a77
Frank Dinoff <frank.dinoff@gmail.com>
2015-06-07 21:13:58 -0400

    Change EmbeddedNeovim stderr to output to logger

--- src/main/java/com/neovim/EmbeddedNeovim.java
+++ src/main/java/com/neovim/EmbeddedNeovim.java
@@ -1,16 +1,24 @@
 package com.neovim;
 
 import com.neovim.msgpack.MessagePackRPC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class EmbeddedNeovim implements MessagePackRPC.Connection {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final Thread thread;
     private Process neovim;
 
     private static List<String> createArgs(String executable, String[] args) {
@@ -23,8 +31,10 @@ public class EmbeddedNeovim implements MessagePackRPC.Connection {
 
     public EmbeddedNeovim(String executable, String... args) throws IOException {
         ProcessBuilder pb = new ProcessBuilder(createArgs(executable, args));
-        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
         neovim = pb.start();
+        thread = new Thread(new StreamLogger(log, neovim.getErrorStream()));
+        thread.setDaemon(true);
+        thread.start();
     }
 
     @Override
@@ -41,12 +51,37 @@ public class EmbeddedNeovim implements MessagePackRPC.Connection {
     public void close() throws IOException {
         try {
             if (neovim.waitFor(1, TimeUnit.SECONDS)) {
-                System.out.println("neovim exited with " + neovim.exitValue());
+                log.info("neovim exited with {}", neovim.exitValue());
+                thread.join();
             } else {
                 neovim.destroy();
             }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static class StreamLogger implements Runnable {
+        private final InputStream stream;
+        private final Logger log;
+
+        public StreamLogger(Logger log, InputStream stream) {
+            this.log = checkNotNull(log);
+            this.stream = checkNotNull(stream);
+        }
+
+        @Override
+        public void run() {
+            try (InputStreamReader input = new InputStreamReader(stream);
+                 BufferedReader reader = new BufferedReader(input)) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    log.error("{}", line);
+                }
+            } catch (IOException ignored) {
+                // The stream will always be closed and once the stream is exhausted there is
+                // nothing left to be printed.
+            }
         }
     }
 }
